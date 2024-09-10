b1ecc0532f7aabdd0c9d99a3a000252c74d4bca1
Syed Jawad Ali <syedjawadali92@gmail.com>
2024-09-10 17:47:06 +0500

    first commit

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+# Base image
+FROM python:3.10-slim
+
+# Set environment variables
+ENV PYTHONDONTWRITEBYTECODE 1
+ENV PYTHONUNBUFFERED 1
+
+# Set work directory
+WORKDIR /app
+
+# Install dependencies
+COPY requirements.txt /app/
+RUN pip install --no-cache-dir -r requirements.txt
+
+# Copy project files to the working directory
+COPY . /app/
+
+# Expose the port the app runs on
+EXPOSE 8000
+
+# Run the application
+CMD ["python", "manage.py", "runserver", "0.0.0.0:8000"]
---
+++ permissions/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# from  e_commerce.settings import AUTH_USER_MODEL
+
+# Create your models here.
+
+class Role(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    permissions = models.ManyToManyField('Permission', related_name='roles')
+    code = models.CharField(max_length=50, null=True, blank=True)
+
+
+class Permission(models.Model):
+    name = models.CharField(max_length=50)
+    description = models.TextField()
+    code = models.CharField(max_length=50)
+    module_name = models.CharField(max_length=50, null=True, blank=True)
+
